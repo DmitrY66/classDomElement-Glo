de921866f8e5b3862e2da5225c0e8e7726aa4e00
Dmitri Gorun <iii448@mail.ru>
2021-08-16 21:45:23 +0500

    lesson14-Inheritance-prototypes-constructors-classes-part-1-fixed

--- script.js
+++ script.js
@@ -13,19 +13,46 @@ DomElement.prototype.createElem = function () {
     let createDiv = document.createElement('div');
     createDiv.className = 'elem';
     createDiv.textContent = "Это созданный блок";
-    createDiv.style.cssText = 'height: 40px; width: 400px; background: green; font-size: 38px; color: white';
+    createDiv.style.cssText = 
+    `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}`;
+    
     document.body.appendChild(createDiv);
   } else if (this.selector === '#elem') {
     let createPar = document.createElement('p');
     createPar.id = 'elem';
     createPar.textContent = "Это созданный параграф";
-    createPar.style.cssText = 'height: 40px; width: 400px; background: blue; font-size: 33px; color: white';
+    createPar.style.cssText = 
+    `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}`;
     document.body.appendChild(createPar);
   }
+
 };
 
-let domElement1 = new DomElement('.elem');
-let domElement2 = new DomElement('#elem');
+let domElement1 = new DomElement('.elem', '50px', '800px', 'LightCoral', '40px');
+let domElement2 = new DomElement('#elem', '80px', '800px', 'LightSeaGreen', '44px');
 
 domElement1.createElem();
 domElement2.createElem();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
